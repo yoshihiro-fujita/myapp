cc9dcd0c73d0bc5a7872b925e75fac5ead7c002b
Yoshihiro Fujita <yoshixxx0010@gmail.com>
2020-08-20 16:59:11 +0900

    finish layout and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
  validates :name, presence: true
  validates :email, presence: true
-validates :password, presence: true、length: {minimum:6}  
+ validates :password, presence: true,length: {minimum:6}  
 
  has_many :vehicles
 end
