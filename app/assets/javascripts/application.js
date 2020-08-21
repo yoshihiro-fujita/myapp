990877fae0f09de1d6a13c68c4935c98e4f77c53
Yoshihiro Fujita <yoshixxx0010@gmail.com>
2020-08-21 13:33:24 +0900

    Implement basic login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-sprockets
 //= require_tree .
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        log_in @user
         format.html { redirect_to @user, notice: 'Welcome to the Myapp.' }
         format.json { render :show, status: :created, location: @user }
       else
